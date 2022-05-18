eaabb28fce1fe585634d50d7d7023b508f62db97
redamahmoud2 <49642562+redamahmoud2@users.noreply.github.com>
2022-05-18 14:40:57 +0200

    Add files via upload

---
+++ assets/js/me.js
@@ -0,0 +1,57 @@
+
+
+
+    // owl carousel
+    $(document).ready(function() {
+ 
+        $("#owl-demo1").owlCarousel({
+
+            autoplay: true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true,
+            items : 3,
+            itemsDesktop : [1199,3],
+            itemsDesktopSmall : [979,3],
+            dots: false
+            
+        });
+       
+      });
+  
+    // end owl carousel
+    // owl carousel
+    $(document).ready(function() {
+ 
+        $("#owl-demo2").owlCarousel({
+
+            autoplay: true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true,
+            items : 3,
+            itemsDesktop : [1199,3],
+            itemsDesktopSmall : [979,3],
+            dots: false
+            
+        });
+       
+      });
+  
+    // end owl carousel
+    // owl carousel
+    $(document).ready(function() {
+ 
+        $("#owl-demo3").owlCarousel({
+
+            autoplay: true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true,
+            items : 3,
+            itemsDesktop : [1199,3],
+            itemsDesktopSmall : [979,3],
+            dots: false
+            
+        });
+       
+      });
+  
+    // end owl carousel
